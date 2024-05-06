1d764418e13ffcd5dda9206e7eb22abf6a5ea204
Teng Li <teng4@ualberta.ca>
2024-05-05 18:31:02 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,22 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  // title: 'Dev Blog', // Required
+  title: 'My Blog', // Required
+  // author: 'Ryan Fitzgerald', // Required
+  author: 'Teng Li', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    // website: 'https://ryanfitzgerald.ca',
+    // github: 'https://github.com/ryanfitzgerald',
+    github: 'https://github.com/teng4',
+    // twitter: 'https://twitter.com/ryanafitzgerald',
+    // linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  // pathPrefix: '/devblog',
+  // siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: '/myblog',
+  siteUrl: 'https://teng4.github.io/myblog/',
 };
